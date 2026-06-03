b751d6f8ae17070e9fa87f482b0ed5036f773250
ghingsabucido-stack <ghingsabucido@gmail.com>
2026-06-03 14:40:04 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,9 @@ app.get("/api/dashboard", (req, res) => {
   });
 });
 
-app.listen(8080, () => {
-  console.log("Backend running on port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
