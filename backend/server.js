a368e36bb8d5ccb0c205500aea972f235124ef22
ghingsabucido-stack <ghingsabucido@gmail.com>
2026-06-05 11:30:20 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // MongoDB
 mongoose.connect("mongodb+srv://admin:ghing1998@cluster0.pnekrww.mongodb.net/inventory")
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("MongoDB Error:", err));
+  .catch(err => console.log(err));
 
 // Model
 const Product = mongoose.model("Product", {
@@ -19,32 +19,18 @@ const Product = mongoose.model("Product", {
 
 // GET
 app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const products = await Product.find();
+  res.json(products);
 });
 
 // POST
 app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.json(product);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// DELETE ALL (optional)
-app.delete("/api/products", async (req, res) => {
-  await Product.deleteMany({});
-  res.json({ message: "Deleted all products" });
+  const product = await Product.create(req.body);
+  res.json(product);
 });
 
 // PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("Server running on " + PORT);
